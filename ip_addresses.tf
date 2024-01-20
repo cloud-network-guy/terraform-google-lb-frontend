ec19a1b1cdb2cec88b383c40264d6de5f96f91d3
cloud-network-guy <greenlakejohnny@gmail.com>
2024-01-19 20:30:01 -0800

    initial push

---
+++ ip_addresses.tf
@@ -0,0 +1,58 @@
+locals {
+  _ip_addresses = [for i, v in local.____forwarding_rules :
+    merge(v, {
+      name        = coalesce(v.address_name, v.name)
+      ip_versions = v.is_internal || v.is_regional ? ["IPV4"] : concat(v.enable_ipv4 ? ["IPV4"] : [], v.enable_ipv6 ? ["IPV6"] : [])
+    })
+  ]
+  ip_addresses = flatten([for i, v in local._ip_addresses :
+    [for ip_version in v.ip_versions :
+      {
+        address_type  = v.is_internal ? "INTERNAL" : "EXTERNAL"
+        name          = v.name
+        is_regional   = v.is_regional
+        region        = v.is_regional ? v.region : "global"
+        subnetwork    = v.subnetwork
+        project_id    = v.project_id
+        prefix_length = v.is_regional ? 0 : null
+        purpose       = v.is_psc ? "GCE_ENDPOINT" : v.is_managed && v.is_internal ? "SHARED_LOADBALANCER_VIP" : null
+        network_tier  = v.is_psc ? null : v.network_tier
+        address       = v.ip_address
+        ip_version    = ip_version
+        index_key     = v.is_regional ? "${v.project_id}/${v.region}/${v.name}" : "${v.project_id}/${v.name}"
+      } if v.create == true || coalesce(v.preserve_ip, false) == true
+    ]
+  ])
+}
+
+# Work-around for scenarios where PSC Consumer Endpoint IP changes
+resource "null_resource" "ip_addresses" {
+  for_each = { for i, v in local.ip_addresses : v.index_key => true if v.purpose == "GCE_ENDPOINT" }
+}
+
+# Regional static IP
+resource "google_compute_address" "default" {
+  for_each      = { for i, v in local.ip_addresses : v.index_key => v if v.is_regional }
+  project       = each.value.project_id
+  name          = each.value.name
+  address_type  = each.value.address_type
+  ip_version    = each.value.ip_version
+  address       = each.value.address
+  region        = each.value.region
+  subnetwork    = each.value.subnetwork
+  network_tier  = each.value.network_tier
+  purpose       = each.value.purpose
+  prefix_length = each.value.prefix_length
+  depends_on    = [null_resource.ip_addresses]
+}
+
+# Global static IP
+resource "google_compute_global_address" "default" {
+  for_each     = { for i, v in local.ip_addresses : v.index_key => v if !v.is_regional }
+  project      = each.value.project_id
+  name         = each.value.name
+  address_type = each.value.address_type
+  ip_version   = each.value.ip_version
+  address      = each.value.address
+}
+
