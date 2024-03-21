0e5883238dfc725b049df6ac2929af54b2204855
cloud-network-guy <greenlakejohnny@gmail.com>
2024-03-21 14:56:37 -0700

    updates

--- target_proxies.tf
+++ target_proxies.tf
@@ -55,11 +55,11 @@ locals {
   __target_https_proxies = [for i, v in local._target_https_proxies :
     merge(v, {
       quic_override = upper(trimspace(coalesce(var.quic_override, "NONE")))
-      ssl_policy = try(coalesce(
+      ssl_policy = coalesce(
         var.existing_ssl_policy,
         v.is_regional ? try(google_compute_region_ssl_policy.default[one(keys(local.ssl_policies))].self_link, null) : null,
-        !v.is_regional ? try(google_compute_ssl_policy.default[one(keys(local.ssl_policies))].self_link, null) : null,
-      ), null)
+        !v.is_regional ? one([for _ in local.ssl_policies : "${local.url_prefix}/${_.project_id}/global/sslPolicies/${_.name}"]) : null,
+      )
       ssl_certificates = concat(
         local.existing_ssl_certs,
         [for _ in local.ssl_certs: "${local.url_prefix}/${_.project_id}/${local.is_regional ? "regions/" : ""}${local.region}/sslCertificates/${_.name}"]
