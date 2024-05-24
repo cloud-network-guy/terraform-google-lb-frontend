e8f153dd0dbc6050c4b07e7c83a9e14e7e7273fd
jheyer <jheyer@opentext.com>
2024-05-24 10:46:02 -0700

    psc fix

--- forwarding_rules.tf
+++ forwarding_rules.tf
@@ -66,7 +66,7 @@ locals {
     merge(v, {
       target                = v.is_psc ? v.target : null              # ? "${local.url_prefix}/${v.target_project_id}/${local.is_regional ? "regions/" : ""}${local.region}/serviceAttachments/${v.target_name}" : v.target
       load_balancing_scheme = v.is_psc ? "" : v.load_balancing_scheme # null doesn't work with PSC forwarding rules
-      subnetwork            = v.is_psc ? "derp" : null                #null : v.is_internal ? local.subnet : null
+      subnetwork            = v.is_psc ? null : v.is_internal ? local.subnet : null
       index_key             = v.is_regional ? "${v.project_id}/${v.region}/${v.name}" : "${v.project_id}/${v.name}"
     }) if v.create == true
   ]
